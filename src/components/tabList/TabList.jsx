4cb47f6d6e7c3ed03230724c90043512cd27899d
kiran-kharsel <kirankharsel@gmail.com>
2026-02-22 09:21:40 +0530

    feat: show tab-content based on tab button click

--- src/components/tabList/TabList.jsx
+++ src/components/tabList/TabList.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
 import Button from '../button'
 
 function TabList({tabs}) {
+  const [selectedIndex, setSelectedIndex] = useState(0)
+
+
+  function handleTabChange(index){
+    return ()=>{
+      setSelectedIndex(index)
+    }
+  };
+
+
   return (
     <div className='tabList'>
       <div className="tabHeader">
         {
           tabs.map((tab, index) => {
-            return <Button key={tab.id} label={tab.label}/>
+            return <Button 
+            key={tab.id} 
+            label={tab.label}
+            onclick={handleTabChange(index)}
+            />
           })
         }
       </div>
-      <div className="tabContent"></div>
+      <div className="tabContent">
+        {
+          tabs[selectedIndex].content
+        }
+      </div>
     </div>
   )
 }
