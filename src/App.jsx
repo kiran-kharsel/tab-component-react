3360fd92a554251f1cf2aa3587988706c140ed67
kiran-kharsel <kirankharsel@gmail.com>
2026-02-22 09:01:48 +0530

    feat: pas tab_config as props to tablist component

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import TabList from './components/tabList/TabList'
 
+import TAB_CONFIG from './constants/tabs.js'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
      <h1>Tabs Component</h1>
-     <TabList/>
+     <TabList tabs={TAB_CONFIG}/>
     </>
   )
 }
