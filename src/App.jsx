8efda63e52d91b5e7c1aaa7e23058907e91d29b5
kiran-kharsel <kirankharsel@gmail.com>
2026-02-22 08:55:14 +0530

    feat: import tablist component in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import TabList from './components/tabList/TabList'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
      <h1>Tabs Component</h1>
+     <TabList/>
     </>
   )
 }
